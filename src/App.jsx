6fdde11a273e6ff926c6cdd14c0ef4d4b8bd57c3
peterbokern <peterbokern@gmail.com>
2024-10-14 18:39:02 +0200

    initial setup of navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import './App.css'
 import Header from "./components/header/Header.jsx";
+import Navigation from './components/navigation/Navigation.jsx';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import CategoryPage from "@/pages/category-page/CategoryPage.jsx";
+import Home from "@/pages/home/Home.jsx";
 
-function App() {
+const App = () => {
+    console.log("hello");
+    return (
+        <>
+            <Router>
+                <Header/>
+                <Navigation/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/category/:categoryName" element={<CategoryPage/>}/>
+                </Routes>
+            </Router>
+        </>
+    );
+};
 
-  return (
-    <>
-        <Header/>
-    </>
-  )
-}
-
-export default App
+export default App;
