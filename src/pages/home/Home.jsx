a59d1f2bb9184b867c09e60b327d3992b59b4b9c
peterbokern <peterbokern@gmail.com>
2024-10-15 22:05:12 +0200

    Added API call to get products from fakestore

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
+import {useProducts} from "@/hooks/useProducts.js";
 
 const Home = () => {
-
+const {data} = useProducts()
+    if (data && data.length) {console.log(data)}
     return (
         <h1>Home</h1>
     );
