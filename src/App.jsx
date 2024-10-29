1dcfdc2008e836b0187746f2d6446b474943c164
peterbokern <peterbokern@gmail.com>
2024-10-29 08:41:05 +0100

    Liked page added and minor styling improvements to product-item componenet

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import CategoryPage from "@/pages/category-page/CategoryPage.jsx";
 import Home from "@/pages/home/Home.jsx";
 import ProductPage from "@/pages/product-page/ProductPage.jsx";
+import LikedPage from "@/pages/liked-page/LikedPage.jsx";
 
 const App = () => {
 
@@ -17,6 +18,7 @@ const App = () => {
                     <Route path="/" element={<Home/>}/>
                     <Route path="/product/:id" element={<ProductPage/>}/>
                     <Route path="/category/:categoryName" element={<CategoryPage/>}/>
+                    <Route path="/likes" element={<LikedPage/>}/>
                 </Routes>
             </Router>
         </>
